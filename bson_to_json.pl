f9100b98b9c275f4bd387c6e14ab68a154ceb260
Brian Mielke <bbmielke@gmail.com>
2017-06-20 09:28:38 -0400

    Saving a javascript based approach

--- bson_to_json.pl
+++ bson_to_json.pl
@@ -5,21 +5,58 @@ use warnings;
 use Mojolicious::Lite;
 use MIME::Base64;
 use Data::Dumper;
-use Mojo::JSON qw(decode_json);
+
+
+get '/' => { template => 'form', input => '', output => ''};
 
 post '/' => sub {
     my $c = shift;
+    my $input = $c->param('input');
+    my $output = $c->param('output');
+
+    my $message;
     eval {
-        my $input = $c->param('input');
-        my $output = decode_base64($input);
-        my $json = decode_json($output);
-        $c->render(json => $json);
+        $output = decode_base64($input);
     };
     if($@) {
-        $c->render(text => "Error: $@", status => 500);
+        $message = "Error: $@";
     }
+    $c->render(template => 'form', input => $input, output => $output, message => $message);
 };
 
 
 app->start;
 
+__DATA__
+
+@@ form.html.ep
+    <!DOCTYPE html>
+        <html>
+        <script>
+          function set_text() {
+
+            var output = `<%= $output %>`;
+            var parser = new DOMParser;
+            var dom = parser.parseFromString(output, 'text/html');
+            var json = JSON.stringify(JSON.parse(dom.body.textContent), undefined, 2);
+            document.getElementById('output').value = json;
+          }
+        </script>
+        <body>
+        <% if(my $message = flash 'message') { %>
+          <p style="color:red;"><%= $message %></p>
+        <% } %>
+        %= form_for '/' => (method => 'post') => begin
+          INPUT: <BR>
+          %= text_area input => '', cols => 80, rows => 15, id => 'input',
+          <BR>
+          OUTPUT: <BR>
+          %= text_area output => '', cols=>80, rows => 25, id => 'output',
+          <BR>
+          %= submit_button 'OK'
+        % end
+        <script>
+          set_text();
+        </script>
+    </body>
+  </html>
